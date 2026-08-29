779abead2524882fe76b8d80ee42d148326ac2be
June Yun <97yunjy@gmail.com>
2026-08-29 01:19:23 -0600

    Running Sum Of An Array

---
+++ RunningSumOfAnArray.js
@@ -0,0 +1,16 @@
+/* 
+Problem : Runing Sum of an Array
+Given an array of nubers called nums, return a new array where each element is the sum of all elements from the beginning of nums up to the current position. 
+*/
+
+function runningSum(nums) {
+  let sumArr = [];
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum = sum + nums[i];
+    sumArr[i] = sum;
+  }
+  return sumArr;
+}
+
+console.log(runningSum([1, 2, 3, 4]));
