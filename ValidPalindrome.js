6e8ae1ea6677d74bf06644956f2c0ac5b005d540
June Yun <97yunjy@gmail.com>
2026-09-05 20:29:45 -0600

    Valid palindrome

---
+++ ValidPalindrome.js
@@ -0,0 +1,25 @@
+/*
+Problem : Given a lowercase string text, return true if it reads the same forward and backward. Otherwise, return false.
+
+Do not create a reversed copy of the string.
+*/
+
+function isPalindrome(text) {
+  let left = 0;
+  let right = text.length - 1;
+
+  while (left < right) {
+    if (text[left] !== text[right]) {
+      return false;
+    } else {
+      left++;
+      right--;
+    }
+  }
+  return true;
+}
+
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("hello"));
+console.log(isPalindrome("aa"));
+console.log(isPalindrome(""));
