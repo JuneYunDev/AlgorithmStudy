1fc1a831f2c670ca0fd28a242b0146a062cb35a2
June Yun <97yunjy@gmail.com>
2026-09-14 20:37:41 -0600

    Complexity Update

--- SearchInsertPosition.js
+++ SearchInsertPosition.js
@@ -27,3 +27,6 @@ console.log(searchInsert([1, 3, 5, 6], 2));
 console.log(searchInsert([1, 3, 5, 6], 7));
 console.log(searchInsert([1, 3, 5, 6], 0));
 console.log(searchInsert([], 4));
+
+//Time Complexity : O(log n)
+//Space Complexity : O(1)
