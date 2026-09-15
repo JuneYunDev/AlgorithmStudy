00fc349ca4aca499046fb557f00e60d61bdef02d
June Yun <97yunjy@gmail.com>
2026-09-14 21:38:15 -0600

    Complexity update

--- MaximumSumOfAFixed-SizeSubarray.js
+++ MaximumSumOfAFixed-SizeSubarray.js
@@ -22,3 +22,10 @@ console.log(maxSubarraySum([2, 1, 5, 1, 3, 2], 3)); // 9
 console.log(maxSubarraySum([4, 2], 2)); // 6
 console.log(maxSubarraySum([-4, -2, -7], 2)); // -6
 console.log(maxSubarraySum([5], 1)); // 5
+
+//Time Complexity : O(n)
+//Space Complexity : O(1)
+
+/*
+I first calculate the sum of the first window. Then, I slide the window by subtracting the value that leaves and adding the new value that enters. I update the maximum sum after each move. The algorithm takes O(n) time and O(1) space.
+*/
